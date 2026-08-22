500c417d9ad0fa06070351cd3a94c93769cedf7e
esnoticias24h <esnoticias24h@gmail.com>
2026-08-22 19:31:49 +0200

    Create config.php

---
+++ admin/php/config.php
@@ -0,0 +1,10 @@
+<?php
+// Configuración de base de datos
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'noticias_db');
+
+$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+if ($conn->connect_error) die("Error de conexión: " . $conn->connect_error);
+?>
